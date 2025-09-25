ce9240d9c91947e780068e7392bd2b809340216d
Elbanteny <m.amiryan.p@gmail.com>
2025-09-25 21:53:31 +0700

    updating api

--- src/app.js
+++ src/app.js
@@ -1,9 +1,53 @@
 import express from 'express';
+import logger from './config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import authroutes from './routes/auth.routes.js';
+import userRoutes from './routes/users.routes.js';
+import securityMiddleware from './middleware/security.middleware.js';
 
 const app = express();
 
+// Diangggap Middleware
+app.use(helmet());
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(securityMiddleware);
+
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
+
 app.get('/', (req, res) => {
+  logger.info('Hello From Acquisitions!');
   res.send('Hello From Acquisitions!');
 });
 
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'ok',
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
+});
+
+app.get('/api', (req, res) => {
+  res.status(200).json({ message: 'Acquisitions Up And Running!' });
+});
+
+app.use('/api/auth', authroutes);
+app.use('/api/users', userRoutes);
+
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
 export default app;
